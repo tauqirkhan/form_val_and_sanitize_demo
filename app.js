4f3005b713b5b656df042e916b2c4ee4ff8a15b3
Tauqir <tauqirkhann.11@gmail.com>
2024-12-27 04:30:01 +0530

    feat: add form app with validation error handling

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("node:path");
+const userRouter = require("./routes/userRouter");
 
 const PORT = process.env.PORT || 3000;
 
@@ -10,10 +11,8 @@ app.use(express.static(assetPath));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use((err, req, res, next) => {
-  console.error(err);
-  res.status(err.statusCode || 500).send(err.message);
-});
+app.use(express.urlencoded({ extended: true }));
+app.use("/", userRouter);
 
 app.listen(PORT, () => {
   console.log(`App is live at - http://localhost:${PORT}/`);
---
+++ controllers/usersController.js
@@ -0,0 +1,49 @@
+const { body, validationResult } = require("express-validator");
+const usersStorage = require("../storages/usersStorage");
+
+const alphaErr = "must only contain letters.";
+const lengthErr = "must be between 1 and 10 characters.";
+
+const validateUser = [
+  body("firstName")
+    .trim()
+    .isAlpha()
+    .withMessage(`First name ${alphaErr}`)
+    .isLength({ min: 1, max: 10 })
+    .withMessage(`First name ${lengthErr}`),
+  body("lastName")
+    .trim()
+    .isAlpha()
+    .withMessage(`Last name ${alphaErr}`)
+    .isLength()
+    .withMessage(`Last name ${lengthErr}`),
+];
+
+exports.usersListGet = (req, res) => {
+  res.render("index", {
+    title: "User list",
+    users: usersStorage.getUsers(),
+  });
+};
+
+exports.usersCreateGet = (req, res) => {
+  res.render("createUser", {
+    title: "Create user",
+  });
+};
+
+exports.usersCreatePost = [
+  validateUser,
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).render("createUser", {
+        title: "Create user",
+        errors: errors.array(),
+      });
+    }
+    const { firstName, lastName } = req.body;
+    usersStorage.addUser({ firstName, lastName });
+    res.redirect("/");
+  },
+];
